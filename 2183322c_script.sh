ee56ad3f9c2152e332200b9f71a6d68ad81c9096
Nur Akmal <mdnurakmal@gmail.com>
2022-08-28 13:28:48 +0800

    Update 2183322c_script.sh

--- 2183322c_script.sh
+++ 2183322c_script.sh
@@ -4,4 +4,5 @@ puppet resource package apache2 ensure=present
 puppet resource service apache2 ensure=running
 
 git clone https://@github.com/mdnurakmal/operate_repo.git
+cd operate_repo
 cp -p index.html /var/www/html
